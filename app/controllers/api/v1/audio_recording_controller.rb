5b30ea9d7e9d842fa6c0586ea6d06c1cb973e069
Rahul Roy <rahulroy1996@gmail.com>
2023-07-19 22:41:21 -0400

    added delete menthod and fixed urls

--- app/controllers/api/v1/audio_recording_controller.rb
+++ app/controllers/api/v1/audio_recording_controller.rb
@@ -1,30 +1,47 @@
 class Api::V1::AudioRecordingController < ApplicationController
+    before_action do
+        ActiveStorage::Current.url_options = { protocol: request.protocol, host: request.host, port: request.port }
+    end
+
     def index
       
       @audio_recordings = AudioRecording.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+
+      @audio_recordings.each do |audio_recording|
+        if audio_recording.audio_data.attached?
+            audio_recording.audio_url = audio_recording.audio_data.url
+        end
+      end
 
       total_pages = @audio_recordings.total_pages
   
       response.headers['X-Total-Pages'] = total_pages.to_s
       render json: @audio_recordings, status: :ok
     end
+
+    def destroy
+        # @audio_recording = AudioRecording.find(params[:id])
+        # @audio_recording.destroy();
+        @audio_recordings = AudioRecording.all()
+        @audio_recordings.each do |audio_recording|
+            if audio_recording.audio_data.attached?
+                audio_recording.destroy()
+            end
+          end
+        render json: {
+            "message": "deleted"
+        }, status: :ok
+    end
   
     def show
         @audio_recording = AudioRecording.find(params[:id])
+        # @audio_recording.audio_url = url_for(@audio_recording.audio_data)
 
-        render json: { 'data': @audio_recording, 'url': url_for(@audio_recording.audio_data) }, status: :ok
+        render json: { 'data': @audio_recording, 'url': @audio_recording.audio_data.url }, status: :ok
     end
 
     def update
         @audio_recording = AudioRecording.find(params[:id])
-
-        # if(params.has_key?(:name))
-        #     @audio_recording.name = params[:name]
-        # elsif (params.has_key?(:category))
-        #     @audio_recording.category = params[:category]
-        # elsif (params.has_key?(:comment))
-        #     @audio_recording.comment = params[:comment]
-        # end
 
         if @audio_recording.update(audio_recording_params)
             render json: { message: "Recording updated successfully", user: @audio_recording }, status: :ok
@@ -39,7 +56,7 @@ class Api::V1::AudioRecordingController < ApplicationController
         require "uri"
         require "net/http"
 
-        url = URI("https://api.openai.com/v1/audio/transcriptions")
+        url = URI(Rails.application.credentials.openai.url)
 
         https = Net::HTTP.new(url.host, url.port)
         https.use_ssl = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :audio_recording, only: [:index, :show, :create, :update]
+      resources :audio_recording, only: [:index, :show, :create, :update, :destroy]
       get '/health_check', to: proc { [200, {}, ['success']] }
     end
   end
