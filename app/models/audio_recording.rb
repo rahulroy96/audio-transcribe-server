e0a782dc5501298436532f7e535aba8cfa72a24f
Rahul Roy <rahulroy1996@gmail.com>
2023-07-19 21:30:22 -0400

    Check for audio type

--- app/models/audio_recording.rb
+++ app/models/audio_recording.rb
@@ -8,9 +8,9 @@ def validate_audio_data
     p "cotent type is "
     p audio_data.content_type
     return unless audio_data.attached?
-    # unless audio_data.content_type.in?(["audio/ogg", "video/quicktime"])
-    #     errors.add(:audio_data, "must be an audio file")
-    # end 
+    unless audio_data.content_type.in?(["audio/mp3", "audio/mp4", "audio/mpga", "audio/mpeg", "audio/m4a", "audio/wav", "audio/webm"])
+        errors.add(:audio_data, "The audio file type must be one of mp3, mp4, mpeg, mpga, m4a, wav, and webm")
+    end 
 
     unless audio_data.blob.byte_size <= 5.megabyte
         errors.add(:audio_data, "is too big")
