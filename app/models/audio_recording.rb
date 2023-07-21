55230f8a85465426a929eff3cd6bd5309ade7c85
Rahul Roy <rahulroy1996@gmail.com>
2023-07-20 20:20:27 -0400

    Added the audio/x-wav to accepted list

--- app/models/audio_recording.rb
+++ app/models/audio_recording.rb
@@ -8,7 +8,7 @@ def validate_audio_data
     p "cotent type is "
     p audio_data.content_type
     return unless audio_data.attached?
-    unless audio_data.content_type.in?(["audio/mp3", "audio/mp4", "audio/mpga", "audio/mpeg", "audio/m4a", "audio/wav", "audio/webm"])
+    unless audio_data.content_type.in?(["audio/mp3", "audio/mp4", "audio/mpga", "audio/mpeg", "audio/m4a", "audio/wav", "audio/webm", "audio/x-wav"])
         errors.add(:audio_data, "The audio file type must be one of mp3, mp4, mpeg, mpga, m4a, wav, and webm")
     end 
 
